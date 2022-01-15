8777964e62faec211abd1d6d511f2cd8c8654e96
Ashish <Ashish@DESKTOP-3UTF11M>
2022-01-15 18:55:36 +0530

    error page for unauthorized user

---
+++ jwtauthentication1/src/main/java/com/jwt/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package com.jwt.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+@Component
+
+//for unauthorized user
+//if the user is not authorized it will throw unauthorized exception
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		
+		response.sendError(401, "Unauthorized...");
+
+	}
+
+}
